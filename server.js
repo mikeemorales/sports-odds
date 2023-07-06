864c36f1cd889b3d7f56c0d6f3183ef5b31867bc
MikeeMorales <j.mikeemorales@gmail.com>
2023-07-06 16:36:14 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const axios = require('axios')
+const app = express();
+const port = process.env.PORT || 9000;
+require('dotenv').config()
+
+app.get('/express_backend', (req, res) => {
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+app.get('/api/odds', async (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://odds.p.rapidapi.com/v4/sports/baseball_mlb/odds',
+        params: {
+            regions: 'us',
+            oddsFormat: 'decimal',
+            markets: 'h2h,spreads',
+            dateFormat: 'iso'
+        },
+        headers: {
+            'X-RapidAPI-Key': process.env.API_KEY,
+            'X-RapidAPI-Host': 'odds.p.rapidapi.com'
+        }
+    };
+    try {
+        const response = await axios.request(options);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+app.listen(9001, () => {
+    console.log(`api data is running on port ${port}/api/odds`);
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const callBackendAPI = async () => {
+    const response = await fetch('/api/odds');
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(data.message);
+    }
+    return data;
+  };
+
+  useEffect(() => {
+    callBackendAPI()
+        .then(res => setData(res.express))
+        .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {data}
+      </div>
   );
 }
 
