d9ae48ab8ce192eba805dd2ff24586ac893afd83
MikeeMorales <j.mikeemorales@gmail.com>
2023-07-16 21:10:38 -0700

    update gamesdatagrid. data grid components

--- src/components/sports/mlb/GamesDataGrid.js
+++ src/components/sports/mlb/GamesDataGrid.js
@@ -3,25 +3,31 @@ import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
     {
-        field: 'firstName',
-        headerName: 'First name',
-        width: 150,
-        editable: true,
+        field: 'matchUp',
+        headerName: 'Match Ups',
+        width: 200
     },
     {
-        field: 'lastName',
-        headerName: 'Last name',
-        width: 150,
-        editable: true,
-    },
-    {
-        field: 'age',
-        headerName: 'Age',
+        field: 'runLine',
+        headerName: 'Run Line',
         type: 'number',
-        width: 110,
-        editable: true,
+        width: 100,
+        editable: false,
+    },
+    {
+        field: 'overUnder',
+        headerName: 'Over / Under',
+        type: 'number',
+        width: 100,
+        editable: false,
+    },
+    {
+        field: 'moneyLine',
+        headerName: 'Money Line',
+        type: 'number',
+        width: 100,
+        editable: false,
     },
     {
         field: 'fullName',
@@ -35,15 +41,12 @@ const columns = [
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, matchUp: 'Dodgers vs. Giants', runLine: 100, overUnder: 100, moneyLine: 100 },
+    { id: 2, matchUp: 'Dodgers vs. Giants', runLine: 100, overUnder: 100, moneyLine: 100 },
+    { id: 3, matchUp: 'Dodgers vs. Giants', runLine: 100, overUnder: 100, moneyLine: 100 },
+    { id: 4, matchUp: 'Dodgers vs. Giants', runLine: 100, overUnder: 100, moneyLine: 100 },
+    { id: 5, matchUp: 'Dodgers vs. Giants', runLine: 100, overUnder: 100, moneyLine: 100 },
+
 ];
 
 const GamesDataGrid = () => {
@@ -60,8 +63,6 @@ const GamesDataGrid = () => {
                     },
                 }}
                 pageSizeOptions={[5]}
-                checkboxSelection
-                disableRowSelectionOnClick
             />
         </Box>
     );
